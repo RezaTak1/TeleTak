48686078f32b24fe9441d68931cc4486e4d110b8
omidtarh <omidtarh@att.net>
2015-11-13 02:01:44 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {tork.wixsrd,70480064},
+    sudo_users = {tork.wizard,70480064},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
