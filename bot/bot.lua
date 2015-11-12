441f9be2417f85291d1dfea0f49e4d5b0a5e4920
omidtarh <omidtarh@att.net>
2015-11-13 03:11:28 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {wezard_omid,147239359},
+    sudo_users = {omidtarh,70480064},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
